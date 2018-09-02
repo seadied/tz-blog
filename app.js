c53085946912b361711c45e4bf728dadeafb2b18
haishi <1254227613@qq.com>
2018-09-02 23:01:58 +0800

    注册登录

---
+++ app.js
@@ -0,0 +1,24 @@
+const Koa = require("koa")
+const views = require("koa-views")
+const static = require("koa-static")
+const logger = require("koa-logger")
+const { join } = require("path")
+const router = require("./routers/router")
+//生成Koa实例
+const app = new Koa
+//注册日志模块
+app.use(logger())
+//配置静态资源目录
+app.use(static(join(__dirname , "public")))
+//配置视图模板
+app.use(views(join(__dirname , "views"), {
+    extension: "pug"
+}))
+
+
+//注册路由信息
+app.use(router.routes()).use(router.allowedMethods())
+
+app.listen(3000, () => {
+    console.log("项目启动成功，监听在3000端口")
+})
---
+++ routers/router.js
@@ -0,0 +1,21 @@
+const Router = require("koa-router")
+const router = new Router
+
+//设计主页
+router.get("/" , async ctx => {
+    await ctx.render("index" , {
+        title : "假装这是一个正经的title"
+    })
+})
+
+//主要用来处理 用户登录 用户注册
+router.get(/^\/user\/(?=reg|login)/, async ctx => {
+    // show 为true 则显示注册 ，为false 显示登录
+    const show = /reg$/.test(ctx.path)
+    
+    await ctx.render("register" ,{show})
+})
+
+
+
+module.exports = router
